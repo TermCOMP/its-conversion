152818f7c631c918baaa67794eac0028573a5ce6
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2025-02-12 09:25:25 +0100

    --help

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 #include <iostream>
 #include <assert.h>
 
+void print_help() {
+    std::cout << "usage: its-conversion --to [ari|koat|smt2] $INPUT.[ari|koat|smt2]" << std::endl;
+    exit(0);
+}
+
 int main(int argc, char *argv[]) {
     bool parse_to {false};
     std::string to, filename;
@@ -14,12 +19,15 @@ int main(int argc, char *argv[]) {
             parse_to = false;
         } else if (strcmp(argv[i], "--to") == 0) {
             parse_to = true;
+        } else if (strcmp(argv[i], "--help") == 0) {
+            print_help();
         } else {
             filename = argv[i];
         }
     }
-    assert(!filename.empty());
-    assert(!to.empty());
+    if (filename.empty() || to.empty()) {
+        print_help();
+    }
     ITS its;
     if (filename.ends_with(".koat")) {
         its = parser::ITSParser::loadFromFile(filename);
@@ -28,7 +36,8 @@ int main(int argc, char *argv[]) {
     } else if (filename.ends_with(".smt2")) {
         its = sexpressionparser::Parser::loadFromFile(filename);
     } else {
-        std::cout << "unknown input format " << std::endl;
+        std::cout << "unknown input format" << std::endl;
+        print_help();
     }
     if (to == "ari") {
         auto ari{its.to_ari()};
@@ -37,12 +46,13 @@ int main(int argc, char *argv[]) {
         }
     } else if (to == "koat") {
         std::cout << its.to_koat();
-    } else if (to == "its") {
+    } else if (to == "smt2") {
         auto res{its.to_its()};
         for (unsigned i = 0; i < res.childCount(); ++i) {
             std::cout << res.getChild(i).toCompactString() << std::endl;
         }
     } else {
         std::cout << "unknown ouput format " << to << std::endl;
+        print_help();
     }
 }
